fb6fafc7a1822537edb5edf0e8ed170b1f44e124
Matthew Masiello <sotek222@yahoo.com>
2019-09-16 10:42:02 -0400

    adds final styling & scripts

--- 05 - Flex Panel Gallery/index.js
+++ 05 - Flex Panel Gallery/index.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-   
+  const panels = document.querySelectorAll(".panel");
+
+  function toggleOpen(){
+    this.classList.toggle("open");
+  }
+
+  function toggleActive(event){
+    if(event.propertyName.includes("flex")){
+      this.classList.toggle("open-active");
+    }
+  }
+  
+  panels.forEach(panel => {
+    panel.addEventListener("click", toggleOpen)   
+    panel.addEventListener("transitionend", toggleActive)   
+  });
 });
