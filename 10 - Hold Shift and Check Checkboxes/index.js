e6d89de9e106b4ea76795047e75885d681190255
Matthew Masiello <sotek222@yahoo.com>
2019-09-30 17:41:30 -0400

    with WesBos

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,41 +1,62 @@
-const inbox = document.querySelector('.inbox');
-const inputs = Array.from(inbox.querySelectorAll('input'));
+// const inbox = document.querySelector('.inbox');
+// const inputs = Array.from(inbox.querySelectorAll('input'));
 
-let clicked = false;
-let shift = false;
-let startBox;
+// let shift = false;
+// let startBox;
 
-inputs.forEach(input => {
-  input.addEventListener('click', e => {
-    if(!shift){
-      startBox = inputs.indexOf(e.target);
-    }
-    if(shift){ 
-      clicked = !clicked;
-      let stopBox = inputs.indexOf(e.target);
-      if(startBox > stopBox){
-        for (let i = stopBox; i < startBox; i++) {
-          inputs[i].checked = true;
-        }
-      } else {
-        for (let i = startBox; i < stopBox; i++) {
-          inputs[i].checked = true; 
-        }
+// inputs.forEach(input => {
+//   input.addEventListener('click', e => {
+//     if(!shift){
+//       startBox = inputs.indexOf(e.target);
+//     }
+//     if(shift){ 
+//       let stopBox = inputs.indexOf(e.target);
+//       if(startBox > stopBox){
+//         for (let i = stopBox; i < startBox; i++) {
+//           inputs[i].checked = true;
+//         }
+//       } else {
+//         for (let i = startBox; i < stopBox; i++) {
+//           inputs[i].checked = true; 
+//         }
+//       }
+//     }
+//   });
+// });
+
+// document.addEventListener('keydown', e => {
+//   if(e.keyCode === 16){
+//     shift = true;
+//     console.log(shift)
+//   }
+// });
+
+// document.addEventListener('keyup', e => {
+//   if (e.keyCode === 16) {
+//     shift = false;
+//     console.log(shift)
+//   }
+// })
+// -------------------------------------WES BOS SOLUTION---------------------------------//
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+
+function handleCheck(e){
+  let inBetween = false; 
+  if(e.shiftKey && this.checked){
+    checkboxes.forEach(cb => {
+      if (cb === this || cb === lastChecked) {
+        inBetween = !inBetween;
+      } 
+      if (inBetween) {
+        cb.checked = true;
       }
-    }
-  });
-});
+    })
 
-document.addEventListener('keydown', e => {
-  if(e.keyCode === 16){
-    shift = true;
-    console.log(shift)
   }
-});
+  lastChecked = this;
+}
 
-document.addEventListener('keyup', e => {
-  if (e.keyCode === 16) {
-    shift = false;
-    console.log(shift)
-  }
-})
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('click', handleCheck);
+});
