3c3c4c8125a4152fa82ba50d417b1c4981b0be74
Matthew Masiello <sotek222@yahoo.com>
2019-10-28 15:17:15 -0400

    update git ignore

--- 20 - Speech Detection/scripts.js
+++ 20 - Speech Detection/scripts.js
@@ -11,10 +11,11 @@ wordsContainer.appendChild(p);
 
 recognition.addEventListener('result', (e) => {
   const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
-    p.textContent = transcript;
+  .map(result => result[0])
+  .map(result => result.transcript)
+  .join('');
+  debugger;
+  p.textContent = transcript;
 
   if (e.results[0].isFinal){
     p = document.createElement('p');
