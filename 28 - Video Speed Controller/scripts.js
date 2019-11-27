d0b7ce5066a886c23119fbd5653a6673b47a850c
Matthew Masiello <sotek222@yahoo.com>
2019-11-26 20:24:49 -0500

    final

--- 28 - Video Speed Controller/scripts.js
+++ 28 - Video Speed Controller/scripts.js
@@ -7,10 +7,28 @@ const bar = speed.querySelector('.speed-bar');
 // function handleMouseMove(e){
 //   const mousePos = e.clientY - this.offsetTop;
 //   const barHeight = this.offsetHeight;
-//   const scrubSpeed = (mousePos / barHeight).toFixed(1) * 2;
+//   const scrubSpeed = (mousePos / barHeight).toFixed(1) * 4;
 //   bar.style.height = `${Math.ceil((mousePos / barHeight) * 100)}%`;
 //   bar.innerText = `${scrubSpeed}x`;
 //   video.playbackRate = scrubSpeed;
 // };
 
 // speed.addEventListener('mousemove', handleMouseMove)
+
+
+function handleStyle(percent, playbackRate){
+  bar.style.height = percent + "%";
+  bar.textContent = playbackRate.toFixed(1) + "x";
+  video.playbackRate = playbackRate;
+};
+
+
+function handleMove(e){
+  const y = e.pageY - this.offsetTop;
+  const percent = ((y / this.offsetHeight) * 100);
+  const min = 0.4, max = 4;
+  const playbackRate = (percent * (max - min) + max) / 100;
+  handleStyle(percent, playbackRate);
+};
+
+speed.addEventListener('mousemove', handleMove);
